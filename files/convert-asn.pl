7a3a68326c7d8c9ecdd2ed0c85f7a55f1061238a
Mats Dufberg <mats.dufberg@iis.se>
2023-07-12 12:24:27 +0000

    Updates for bgp.tools data

---
+++ files/convert-asn.pl
@@ -0,0 +1,137 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+use feature ('say');
+use Net::IP;
+
+# PURPOSE
+# This script will, after postprocessing, create two data files for rbldnsd
+# for ASN lookup over DNS in Cymru format, one for IPv4 and one for IPv6.
+
+# INPUT DATA
+# Input, standard input, to this script is the ASN data file from
+# BGP.Tools (https://bgp.tools/) in text format (https://bgp.tools/table.txt).
+# The data file has the following format:
+#
+# <Prefix><Space><ASN>
+#
+# Prefix is an IPv4 prefix or an IPv6 prefix.
+#
+# The IPv4 prefix is in the usual IPv4 display format (four dot-separated octets)
+# plus "/" plus a prefix length 0-32 bits.
+#
+# The IPv6 prefix is in the usual IPv6 display format plus a prefix lenght 0-64
+# bits
+#
+# Space is an ASCII x2D space character.
+#
+# ASN is a single ASN code.
+
+# OUTPUT DATA
+# This script will create a meta file on standard output. In the meta file every
+# line is prepended with "IPV4"+<TAB> or "IPV6"+<TAB>. Two data files can be
+# created from the meta file, one for IPv4 and one for IPv6 with the following
+# commands assuing that the name of the metafile is "asndata.txt":
+#
+# grep "^IPV4" asndata.txt | cut -f2- > ipv4data.txt
+# grep "^IPV6" asndata.txt | cut -f2- > ipv6data.txt
+
+
+# Print header, one for the IPv4 data file and one for the IPv6 data file. Prefix
+# "IPV4" and "IPV6", respektively, to make it easy to separate IPv6 from IPv4 in
+# postprocessing step after this script has been run.
+
+say 'IPV6' . "\t" . '# ------------------------------------------';
+say 'IPV6' . "\t" . '$DATASET dnset origin6';
+say 'IPV6' . "\t" . '$TTL 14400';		    
+say 'IPV6' . "\t" . '# ------------------------------------------';
+
+say 'IPV4' . "\t" . '# ------------------------------------------';
+say 'IPV4' . "\t" . '$DATASET ip4trie origin';
+say 'IPV4' . "\t" . '$TTL 14400';
+say 'IPV4' . "\t" . '# ------------------------------------------';
+
+my ($linecnt) = 0; # Line counter
+
+while (<>) {
+
+    $linecnt++;
+    
+    chomp;
+
+    my ($ip, $prefix, $asn, $rev, $lenfulldigit, $subrev, $remainder);
+    
+    if (m!^([0-9a-f]+[0-9a-f:]+)/([0-9]+) +([0-9]+) *$!) {
+	# IPv6 prefix plus ASN
+	
+	$ip = $1;      # IP address part
+	$prefix = $2;  # Prefix lenght
+	$asn = $3;     # ASN code
+
+	# Sanitary tests
+	die qq(ERROR on line $linecnt in input data: "$ip" is not an IPv6 address) unless Net::IP::ip_is_ipv6($ip);
+	die qq(ERROR on line $linecnt in input data: "$prefix" is no a valid IPv6 subnet mask >= 0 and <= 64)
+	    unless $prefix =~ /^\d+$/ and $prefix >= 0 and $prefix <= 64;
+
+	# Create a reverse name from address 
+	$rev = Net::IP::ip_reverse ($ip);
+	die qq(ERROR on line $linecnt in input data: Reverse failed on "$ip") unless $rev;
+	$rev =~ s/\.ip6\.arpa\.?$//; # remove ".ip.arpa." from reverse name
+
+	
+	# Every digit in the IP address covers 4 bits. Find the number digits, with the
+	# prefix that covers whole digits, e.g. both 4 and 5 bits cover one digit, but
+	# 5 bits has then a remainder of 1 bit handled below.
+	#
+	# Before each digit in the reverse we keep the dot, i.e. "* 2".
+	$lenfulldigit = int ($prefix /4) * 2; # Counted from the end of $rev below
+	$subrev = substr $rev, -$lenfulldigit;
+	$remainder = $prefix % 4;
+	
+	# Prefix with "IPV6\t". Print TXT information for following reverses
+	say "IPV6\t:127.0.0.2:$asn | $ip/$prefix | NA | NA | NA";
+
+	if ($remainder == 0) {
+	    # Prefix lenght was 0, 4, 8...
+	    say "IPV6\t$subrev";
+
+	} else {
+	    # Get the next digit after the substring extracted
+	    my $nextdigit = hex substr $rev, -$lenfulldigit-1, 1;
+	    # Governs how many subnets to match
+	    my ($addon);
+	    $addon = 7 if $remainder == 1; # 0-6
+	    $addon = 3 if $remainder == 2; # 0-2
+	    $addon = 1 if $remainder == 3; # 0
+
+	    for (my $i = 0; $i <= $addon; $i++) {
+		my $add = $nextdigit + $i;
+		die qq(ERROR on line $linecnt in input data: Calculated digit "$add" is greater than 15") if $add > 15;
+		say 'IPV6' . "\t" . '.' . sprintf ("%x", $add) . $subrev;
+	    }
+	}
+	    
+    } elsif (m!^(\d+\.\d+\.\d+\.\d+)/([0-9]+) +([0-9]+) *$!) {
+
+	$ip = $1;      # IP address part
+	$prefix = $2;  # Prefix lenght
+	$asn = $3;     # ASN code
+
+	# Sanitary tests
+	die qq(ERROR on line $linecnt in input data: "$ip" is not an IPv4 address) unless Net::IP::ip_is_ipv4($ip);
+	die qq(ERROR on line $linecnt in input data: "$prefix" is no a valid IPv4 subnet mask >= 0 and <= 24)
+	    unless $prefix =~ /^\d+$/ and $prefix >= 0 and $prefix <= 24;
+
+	say "IPV4\t$ip/$prefix:127.0.0.2:$asn | $ip/$prefix | NA | NA | NA";
+	    
+    } elsif (m/^\s*$/) {
+
+	# Ignore empty line
+	
+    } else {
+	
+	die qq(ERROR on line $linecnt in input data);
+    }
+}
+
---
+++ files/fetch.sh
@@ -0,0 +1,211 @@
+#! /usr/bin/env bash
+
+# Directories used in this script
+basedir="/var/lib/rbldns"
+datadir="$basedir/data"
+logdir="$basedir/log"
+scriptdir="$basedir/script"
+zonedir="$basedir/zones"
+
+# Script run from this script
+convertscript="$scriptdir/convert-asn.pl"
+
+# Files
+sourcefile="table.txt" # Stored in datadir
+metafile="$datadir/tablemetadata.txt"
+ipv4data="$zonedir/ipv4data.txt"  # Must have the same path and names
+ipv6data="$zonedir/ipv6data.txt"  # as specified for rbldnsd
+
+# For fetching the source file
+url="https://bgp.tools/$sourcefile"
+wgetcommand="wget --directory-prefix=$datadir -o $logdir/wget.log --backups=30 $url"
+
+
+logmessage()
+{
+    # For sending messages to syslog. First argument must be ERROR or INFO.
+    # Second argument must be a string (the message)
+    
+    local level facil
+    level=$1
+    shift
+
+    if [ "$level" = ERROR ]; then
+	facil="daemon.err"
+	
+    elif [ "$level" = WARNING ]; then
+	facil="daemon.warning"
+
+    elif [ "$level" = INFO ]; then
+	facil="daemon.info"
+
+    else
+	# Error in code
+	exit 1
+    fi
+    logger --id=$$ -p $facil $@
+}
+
+
+set_lock_and_trap ()
+{
+    # Lock and set trap to delete lock files.
+
+    local lockfile tmplockfile lockingpid
+
+    lockfile="/tmp/rbldns-fetch.lock"
+
+    # Create temporary lock file from template
+    if ! tmplockfile=$( mktemp $lockfile.XXXXXX ) ; then
+	logmessage ERROR "Cannot create temporary lock file. TERMINATING."
+	return 1
+    fi
+
+    # Add message to temporary lock file
+    if ! (echo "Locked at $(date) with pid"; echo "$$") > $tmplockfile ; then
+	logmessage ERROR "Cannot add text to temporary lock file. TERMINATING."
+	rm -f $tmplockfile
+	return 1
+    fi
+
+    # Lock
+    if ln $tmplockfile $lockfile 2>/dev/null; then
+	rm -f $tmplockfile
+    else
+	rm -f $tmplockfile
+	lockingpid=$(tail -1 $lockfile)
+	logmessage ERROR "Process already locked by pid '$lockingpid'."
+	if echo $lockingpid | egrep -q "^[0-9][0-9]*$" ; then
+	    if [ -z "$(ps -h -p $lockingpid)" ] ; then
+		rm -f $lockfile
+		logmessage INFO "No process behind locking pid. Lock file removed."
+	    fi
+	fi
+	return 1
+    fi
+
+    # Set traps to have lock file cleared at program termination
+    trap "rm -f $lockfile; exit 0" 0 || exit 1
+    trap "rm -f $lockfile; exit 1" 1 2 3 6 15 || exit 1   
+} # END set_lock_and_trap ()
+
+
+fetchsource()
+{
+    # Fetch a new source file
+
+    local newfile newfilelength oldfile oldfilelength
+
+    newfile="$datadir/$sourcefile"
+    oldfile="$datadir/$sourcefile.1"
+
+    if $wgetcommand ; then
+
+	# New file has been fetch
+
+	newfilelength=$(wc -l < $newfile) # Number of lines in the fetched file
+	if ! [ -s $newfile ] ; then
+	    logmessage ERROR "Fetched $sourcefile is empty. TERMINATING."
+	    return 1
+	fi	 
+	logmessage INFO "Fetched $sourcefile with $newfilelength lines."
+	
+	# Old file must exist and be non-empty to be able to be compared to
+	if [ -s $oldfile ] ; then
+	    oldfilelength=$(wc -l < $oldfile) # Number of lines in previous file
+	    if [ "$(( 10 * newfilelength / oldfilelength ))" -lt 9 ] ; then
+		logmessage WARNING "Fetched $sourcefile has decrease more than 10% compared to previous version."
+	    fi
+	fi
+    else
+	logmessage ERROR "Fetching $sourcefile failed. TERMINATING."
+	return 1
+    fi
+    return 0      
+}
+
+convertsource()
+{
+    # Convert the fetched soruce file to a meta format
+
+    local scipterror newfile
+
+    newfile="$datadir/$sourcefile"
+
+    # Save if exists and is non-empty
+    if [ -s $metafile ]; then
+	mv $metafile $metafile.1
+    fi
+    
+    scripterror=$( cat $newfile | $convertscript 2>&1 > $metafile)
+
+    # If there was an error, send a log message and terminate
+    if [ -n "$scripterror" ] ; then
+	logmessage ERROR "$scripterror"
+	return 1
+    fi
+
+    if ! [ -s $metafile ]; then
+	logmessage ERROR "Created metafile is empty. TERMINATING."
+	return 1
+    fi
+    return 0
+}
+
+createzonefiles()
+{
+    # Create the IPv4 and IPv6 zone files from the metafile
+
+    local ipv4datatmp ipv6datatmp
+
+    ipv4datatmp="$ipv4data.tmp"
+    ipv6datatmp="$ipv6data.tmp"
+
+    
+    if ! grep "^IPV4" $metafile | cut -f2- > $ipv4datatmp ; then
+	logmessage ERROR "Creating IPv4 zone file failed. TERMINATING."
+	return 1
+    fi
+	
+    if ! grep "^IPV6" $metafile | cut -f2- > $ipv6datatmp ; then
+	logmessage ERROR "Creating IPv6 zone file failed. TERMINATING."
+	return 1
+    fi
+	
+    # Move files into place. Should not create error
+    chmod 644 $ipv4datatmp
+    chmod 644 $ipv6datatmp
+    if ! mv $ipv4datatmp $ipv4data; then
+	logmessage ERROR "Creating $(basename $ipv4data) failed. TERMINATING."
+	return 1
+    fi
+    if ! mv $ipv6datatmp $ipv6data; then
+	logmessage ERROR "Creating $(basename $ipv6data) failed. TERMINATING."
+	return 1
+    fi
+}
+    
+# Start
+logmessage INFO "$(basename $0) started"
+
+if ! set_lock_and_trap ; then
+    logmessage ERROR "$(basename $0) terminated unsucessfully"
+    exit 1
+fi
+
+if ! fetchsource ; then
+    logmessage ERROR "$(basename $0) terminated unsucessfully"
+    exit 1
+fi
+if ! convertsource ; then
+    logmessage ERROR "$(basename $0) terminated unsucessfully"
+    exit 1
+fi
+if ! createzonefiles ; then
+    logmessage ERROR "$(basename $0) terminated unsucessfully"
+    exit 1
+fi
+
+logmessage INFO "$(basename $0) completed sucessfully"
+
+
