f03a741a5e8ccc2d8e9e5648193af83c987bd011
Mats Dufberg <mats.dufberg@iis.se>
2023-09-19 16:52:49 +0200

    Updates convert-asn.pl
    * Makes the script ignore lines with prefix /25-32 (IPv4) or
      /65-128 (IPv6)
    * Makes the script count ignored lines
    * Makes the script output a message to STDERR but not terminating
    * Other editorial updates

--- files/convert-asn.pl
+++ files/convert-asn.pl
@@ -42,9 +42,18 @@ use Net::IP;
 # "IPV4" and "IPV6", respektively, to make it easy to separate IPv6 from IPv4 in
 # postprocessing step after this script has been run.
 
+# STDERR
+# If prefixes /25-32 (IPv4) or /65-128 (IPv6) are found, those are ignored and
+# counted. At the end a warning is printed to STDERR.
+#
+# If other errors are found in the input data, then an error message is printed
+# to standard error, the script is terminated and and a non-zero result code is
+# returned to the calling code.
+
+
 say 'IPV6' . "\t" . '# ------------------------------------------';
 say 'IPV6' . "\t" . '$DATASET dnset origin6';
-say 'IPV6' . "\t" . '$TTL 14400';		    
+say 'IPV6' . "\t" . '$TTL 14400';
 say 'IPV6' . "\t" . '# ------------------------------------------';
 
 say 'IPV4' . "\t" . '# ------------------------------------------';
@@ -54,32 +63,42 @@ say 'IPV4' . "\t" . '# ------------------------------------------';
 
 my ($linecnt) = 0; # Line counter
 
+my ($ignorev4cnt) = 0; # Counting ignored IPv4 lines with prefix /25-32
+my ($ignorev6cnt) = 0; # Counting ignored IPv6 lines with prefix /65-128
+
+
 while (<>) {
 
     $linecnt++;
-    
+
     chomp;
 
     my ($ip, $prefix, $asn, $rev, $lenfulldigit, $subrev, $remainder);
-    
+
     if (m!^([0-9a-f]+[0-9a-f:]+)/([0-9]+) +([0-9]+) *$!) {
 	# IPv6 prefix plus ASN
-	
+
 	$ip = $1;      # IP address part
 	$prefix = $2;  # Prefix lenght
 	$asn = $3;     # ASN code
 
 	# Sanitary tests
 	die qq(ERROR on line $linecnt in input data: "$ip" is not an IPv6 address) unless Net::IP::ip_is_ipv6($ip);
-	die qq(ERROR on line $linecnt in input data: "$prefix" is no a valid IPv6 subnet mask >= 0 and <= 64)
-	    unless $prefix =~ /^\d+$/ and $prefix >= 0 and $prefix <= 64;
+	die qq(ERROR on line $linecnt in input data: "$prefix" is not a valid IPv6 prefix within range 0 to 128)
+	    unless $prefix =~ /^\d+$/ and $prefix >= 0 and $prefix <= 128;
 
-	# Create a reverse name from address 
+	# Count and ignore prefix /65-128
+	if ($prefix >= 65 and $prefix <= 128) {
+	    $ignorev6cnt++;
+	    next;
+	};
+
+	# Create a reverse name from address
 	$rev = Net::IP::ip_reverse ($ip);
 	die qq(ERROR on line $linecnt in input data: Reverse failed on "$ip") unless $rev;
 	$rev =~ s/\.ip6\.arpa\.?$//; # remove ".ip.arpa." from reverse name
 
-	
+
 	# Every digit in the IP address covers 4 bits. Find the number digits, with the
 	# prefix that covers whole digits, e.g. both 4 and 5 bits cover one digit, but
 	# 5 bits has then a remainder of 1 bit handled below.
@@ -88,7 +107,7 @@ while (<>) {
 	$lenfulldigit = int ($prefix /4) * 2; # Counted from the end of $rev below
 	$subrev = substr $rev, -$lenfulldigit;
 	$remainder = $prefix % 4;
-	
+
 	# Prefix with "IPV6\t". Print TXT information for following reverses
 	say "IPV6\t:127.0.0.2:$asn | $ip/$prefix | NA | NA | NA";
 
@@ -111,7 +130,7 @@ while (<>) {
 		say 'IPV6' . "\t" . '.' . sprintf ("%x", $add) . $subrev;
 	    }
 	}
-	    
+
     } elsif (m!^(\d+\.\d+\.\d+\.\d+)/([0-9]+) +([0-9]+) *$!) {
 
 	$ip = $1;      # IP address part
@@ -120,18 +139,33 @@ while (<>) {
 
 	# Sanitary tests
 	die qq(ERROR on line $linecnt in input data: "$ip" is not an IPv4 address) unless Net::IP::ip_is_ipv4($ip);
-	die qq(ERROR on line $linecnt in input data: "$prefix" is no a valid IPv4 subnet mask >= 0 and <= 24)
-	    unless $prefix =~ /^\d+$/ and $prefix >= 0 and $prefix <= 24;
+	die qq(ERROR on line $linecnt in input data: "$prefix" is not a valid IPv4 prefix within range 0 to 32)
+	    unless $prefix =~ /^\d+$/ and $prefix >= 0 and $prefix <= 32;
+
+	# Count and ignore prefix /25-32
+	if ($prefix >= 25 and $prefix <= 32) {
+	    $ignorev4cnt++;
+	    next;
+	};
 
 	say "IPV4\t$ip/$prefix:127.0.0.2:$asn | $ip/$prefix | NA | NA | NA";
-	    
+
     } elsif (m/^\s*$/) {
 
 	# Ignore empty line
-	
+
     } else {
-	
+
 	die qq(ERROR on line $linecnt in input data);
     }
+}
+
+# Print warning to STDERR if any of the ignore counters are greater than zero.
+if ($ignorev4cnt > 0 and $ignorev6cnt > 0) {
+    warn qq(Ignored $ignorev4cnt lines with IPv4 prefix /25-32 and $ignorev6cnt lines with IPv6 prefix /65-128) . "\n";
+} elsif ($ignorev4cnt > 0) {
+    warn qq(Ignored $ignorev4cnt lines with IPv4 prefix /25-32) . "\n";
+} elsif ($ignorev6cnt > 0) {
+    warn qq($ignorev6cnt lines with IPv6 prefix /65-128) . "\n";
 }
 
